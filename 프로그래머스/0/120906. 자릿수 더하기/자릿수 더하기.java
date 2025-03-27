b7de757fe62f11c8e30bf521d1fb3ac93654241b
sanggyoon <158446172+sanggyoon@users.noreply.github.com>
2025-03-27 10:40:27 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String number = String.valueOf(n);
+        
+        for (int i=0; i<number.length(); i++){
+            answer += number.charAt(i) - '0';
+        }
+        return answer;
+    }
+}
