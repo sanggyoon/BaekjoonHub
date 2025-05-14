a1e311398fa71ecbd5b1bae49dfcbac85fdfccd6
sanggyoon <158446172+sanggyoon@users.noreply.github.com>
2025-05-14 22:56:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        int cards1Index = 0;
+        int cards2Index = 0;
+        
+        for (int i = 0; i<goal.length; i++){
+            if ( cards1Index < cards1.length && goal[i].equals(cards1[cards1Index])){
+                cards1Index++;
+            } else if (cards2Index < cards2.length && goal[i].equals(cards2[cards2Index])){
+                cards2Index++;
+            } else {
+                return answer = "No";
+            }
+        }
+        return answer = "Yes";
+    }
+}
